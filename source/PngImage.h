01575039f493a7b9a00f7b8c5634b9528279bd2c
DESKTOP-0LF84UV\Leevi <lpkknn@gmail.com>
2019-05-13 22:17:50 +0300

    added pngimage to the project

---
+++ source/PngImage.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define PNG_DEBUG 3
+#include <png.h>
+
+class PngImage
+{
+public:
+	int width, height;
+
+	PngImage(void);
+	void OpenImage(char* location);
+
+private:
+	int x, y;
+	png_byte color_type;
+	png_byte bit_depth;
+	
+	png_structp png_ptr;
+	png_infop info_ptr;
+	int number_of_passes;
+	png_bytep * row_pointers;
+};
+
